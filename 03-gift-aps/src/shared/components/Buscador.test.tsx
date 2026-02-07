ec9fe08e1916352d454e81b9561fe5f3dca1df1e
¨EdWin5238¨ <edwin.echeverria.102@gmail.com>
2026-02-07 02:44:18 -0700

    tercera actualizacion del proyecto

--- 03-gift-aps/src/shared/components/Buscador.test.tsx
+++ 03-gift-aps/src/shared/components/Buscador.test.tsx
@@ -4,7 +4,7 @@ import { Buscador } from "./Buscador";
 
 describe('Buscador', () => {
     test('deberia renderisar la barra de busqueda correctamente', () => {
-        const { container } = render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={() => { }} />)
+        const { } = render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={() => { }} />)
 
         //expect(container).toMatchSnapshot()
         expect(screen.getByRole('textbox')).toBeDefined()
@@ -13,16 +13,56 @@ describe('Buscador', () => {
 
     test('deberia llamar al manejo buscar con los correctos valores despues de 700ms', async () => {
         const manejobuscar = vi.fn()
-        render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={() => { }} />)
+        render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={manejobuscar} />)
 
         const input = screen.getByRole('textbox')
-        fireEvent.change(input, { taget: { value: 'test' } })
+        fireEvent.change(input, { target: { value: 'test' } })
 
         await new Promise((resolve) => setTimeout(resolve, 701))
 
-        //await waitFor(() => {
-        expect(manejobuscar).toHaveBeenCalled()
+        //espera a que pasen los eventos
+        await waitFor(() => {
+            expect(manejobuscar).toHaveBeenCalled()
+            expect(manejobuscar).toHaveBeenCalledWith('test')
+        })
+    })
+
+    test('deberia llamarse una vez cuando dejas de escribir', async () => {
+        const manejobuscar = vi.fn()
+        render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={manejobuscar} />)
+
+        const input = screen.getByRole('textbox')
+        fireEvent.change(input, { target: { value: 't' } })
+        fireEvent.change(input, { target: { value: 'te' } })
+        fireEvent.change(input, { target: { value: 'tes' } })
+        fireEvent.change(input, { target: { value: 'test' } })
+
+        await waitFor(() => {
+            expect(manejobuscar).toHaveBeenCalledTimes(1)
+            expect(manejobuscar).toHaveBeenCalledWith('test')
+        })
+    })
+
+    test('deberia llamarse el manejo buscar cuando boton es precionado', () => {
+        const manejobuscar = vi.fn()
+        render(<Buscador buscador="buscar gif" boton="buscar" manejobuscar={manejobuscar} />)
+
+        const input = screen.getByRole('textbox')
+        fireEvent.change(input, { target: { value: 'test' } })
+
+        const button = screen.getByRole('button')
+
+        fireEvent.click(button)
+
+        expect(manejobuscar).toHaveBeenCalledTimes(1)
         expect(manejobuscar).toHaveBeenCalledWith('test')
-        //})
+    })
+
+    test('el buscador deberia estar bien posicionado', () => {
+        const valor = 'buscar gift'
+        render(<Buscador buscador={valor} boton="buscar" manejobuscar={() => { }} />)
+
+        expect(screen.getByPlaceholderText(valor)).toBeDefined()
+
     })
 })
